52fc539e8e4d7e4deaf52de8a734b8fe3b775b10
Fernando Arruza <f-arruza>
2016-10-29 09:14:46 -0500

    Creación de entidades EmpresaEmpleador y Pensionado

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import EmpresaEmpleadora, Pensionado
 
 # Register your models here.
+@admin.register(EmpresaEmpleadora)
+class EmpresaEmpleadoraAdmin(admin.ModelAdmin):
+    search_fields = ('nombreRazonSocial', 'tipoDocumento',
+        'numeroDocumento', )
+
+@admin.register(Pensionado)
+class PensionadoAdmin(admin.ModelAdmin):
+    search_fields = ('tipoDocumento', 'numeroDocumento', )
