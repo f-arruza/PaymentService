0645860a4a1a7e0eaca66e8fa23a96c6f2b4b85c
Fernando Arruza <f-arruza>
2016-11-05 23:17:51 -0500

    Install gunicorn

--- paymentservice/urls.py
+++ paymentservice/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^manager/', include('payment.urls')),
+    url(r'^payment/', include('payment.urls')),
 ]
