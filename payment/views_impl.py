0fe17d9b544c059d2d8623a735bca15b47ab0238
Fernando Arruza <f-arruza>
2016-11-06 12:13:12 -0500

    Fix views_impl.py

--- payment/views_impl.py
+++ payment/views_impl.py
@@ -6,29 +6,29 @@ from .views import (EmpresaEmpleadoraListAPIView,
 
 class EmpresaEmpleadoraListViewImpl(EmpresaEmpleadoraListAPIView):
     def __init__(self):
-        EmpresaEmpleadoraListView.__init__(self)
+        EmpresaEmpleadoraListAPIView.__init__(self)
 
 
 class EmpresaEmpleadoraCreateViewImpl(EmpresaEmpleadoraCreateAPIView):
     def __init__(self):
-        EmpresaEmpleadoraCreateView.__init__(self)
+        EmpresaEmpleadoraCreateAPIView.__init__(self)
 
 
 class EmpresaEmpleadoraUpdateViewImpl(EmpresaEmpleadoraUpdateAPIView):
     def __init__(self):
-        EmpresaEmpleadoraUpdateView.__init__(self)
+        EmpresaEmpleadoraUpdateAPIView.__init__(self)
 
 
 class PensionadoListViewImpl(PensionadoListAPIView):
     def __init__(self):
-        PensionadoListView.__init__(self)
+        PensionadoListAPIView.__init__(self)
 
 
 class PensionadoCreateViewImpl(PensionadoCreateAPIView):
     def __init__(self):
-        PensionadoCreateView.__init__(self)
+        PensionadoCreateAPIView.__init__(self)
 
 
 class PensionadoUpdateViewImpl(PensionadoUpdateAPIView):
     def __init__(self):
-        PensionadoUpdateView.__init__(self)
+        PensionadoUpdateAPIView.__init__(self)
