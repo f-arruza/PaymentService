c65f4e0d951362e4e665fce7493805d3806d0f57
f-arruza <f.arruza@uniandes.edu.co>
2016-11-08 23:52:31 -0500

    Fix import

--- payment/models.py
+++ payment/models.py
@@ -2,8 +2,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
 from payment.implements.validations_impl import ValidationsImpl
+from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
